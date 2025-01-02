6fa7dd8b31b8c98fd989d82548a792fbbade8ba8
tristan-gajsek <tristan.gajsek@student.um.si>
2025-01-02 05:09:36 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default =
+        with pkgs;
+        mkShell rec {
+          shellHook = "exec zsh";
+          buildInputs = [
+            mpi
+            mpi.dev
+            pkg-config
+          ];
+          nativeBuildInputs = buildInputs;
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        };
+    };
+}
