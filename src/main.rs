3d6f8dd058c3bf9222e30da124aeafcaf6056a61
tristan-gajsek <tristan.gajsek@student.um.si>
2025-01-02 07:21:52 +0100

    Add itertools and input parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::io::{self, BufRead, Read};
+
 use anyhow::{Ok, Result};
 use clap::Parser;
 use cli::Cli;
 use colored::Colorize;
+use itertools::Itertools;
 
 mod cli;
 
@@ -14,5 +17,31 @@ fn main() {
 }
 
 fn run(args: &Cli) -> Result<()> {
+    println!("{:#?}", read_input(args.size));
     Ok(())
+}
+
+fn read_input(size: Option<usize>) -> Result<Box<[Box<[u8]>]>> {
+    let mut stdin = io::stdin().lock();
+
+    let chunks = if let Some(size) = size {
+        let mut input = vec![];
+        stdin.read_to_end(&mut input)?;
+        input
+            .into_iter()
+            .chunks(size)
+            .into_iter()
+            .map(|chunk| chunk.collect())
+            .collect()
+    } else {
+        stdin
+            .lines()
+            .map(|line| {
+                line.map(|line| line.into_bytes().into())
+                    .map_err(anyhow::Error::from)
+            })
+            .collect::<Result<_>>()?
+    };
+
+    Ok(chunks)
 }
