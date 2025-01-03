dce085c4a80b4702275091a0a01bec57e3c4e747
tristan-gajsek <tristan.gajsek@student.um.si>
2025-01-03 09:09:58 +0100

    Make Huffman Node struct more idiomatic

--- src/huffman.rs
+++ src/huffman.rs
@@ -8,30 +8,30 @@ use bit_vec::BitVec;
 #[derive(Debug, PartialEq, Eq)]
 struct Node {
     value: u32,
-    byte: Option<u8>,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+    content: NodeContent,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum NodeContent {
+    Byte(u8),
+    Leaves(Option<Box<Node>>, Option<Box<Node>>),
 }
 
 impl Node {
     pub fn new(value: u32, byte: u8) -> Self {
         Self {
             value,
-            byte: Some(byte),
-            left: None,
-            right: None,
+            content: NodeContent::Byte(byte),
         }
     }
 
     pub fn new_tree(mut counts: BinaryHeap<Node>) -> Self {
         while counts.len() > 1 {
-            let right = counts.pop().unwrap();
             let left = counts.pop().unwrap();
+            let right = counts.pop().unwrap();
             let node = Node {
                 value: left.value + right.value,
-                byte: None,
-                left: Some(Box::new(left)),
-                right: Some(Box::new(right)),
+                content: NodeContent::Leaves(Some(Box::new(left)), Some(Box::new(right))),
             };
             counts.push(node);
         }
@@ -44,13 +44,19 @@ impl Node {
         codes
     }
 
-    fn get_codes_inner(&self, mut code: BitVec, codes: &mut HashMap<u8, BitVec>) {
-        self.byte.map(|b| codes.insert(b, code.clone()));
-        let mut code2 = code.clone();
-        code.push(false);
-        code2.push(true);
-        self.left.as_ref().map(|l| l.get_codes_inner(code, codes));
-        self.right.as_ref().map(|r| r.get_codes_inner(code2, codes));
+    fn get_codes_inner(&self, code: BitVec, codes: &mut HashMap<u8, BitVec>) {
+        match &self.content {
+            NodeContent::Byte(byte) => {
+                codes.insert(*byte, code.clone());
+            }
+            NodeContent::Leaves(left, right) => {
+                let (mut lcode, mut rcode) = (code.clone(), code);
+                lcode.push(false);
+                rcode.push(true);
+                left.as_ref().map(|l| l.get_codes_inner(lcode, codes));
+                right.as_ref().map(|r| r.get_codes_inner(rcode, codes));
+            }
+        }
     }
 
     pub fn get_byte(&self, bits: &BitVec) -> u8 {
@@ -66,10 +72,7 @@ impl PartialOrd for Node {
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> Ordering {
-        other
-            .value
-            .cmp(&self.value)
-            .then(other.byte.cmp(&self.byte))
+        other.value.cmp(&self.value)
     }
 }
 
@@ -80,12 +83,10 @@ pub fn compress(input: &[u8]) -> Box<[u8]> {
         .for_each(|byte| *counts.entry(byte).or_insert(0u32) += 1);
     let tree = Node::new_tree(
         counts
-            .into_iter()
-            .map(|(byte, count)| Node::new(count, *byte))
+            .iter()
+            .map(|(byte, count)| Node::new(*count, **byte))
             .collect(),
     );
-    dbg!(&tree);
-    dbg!(tree.get_codes());
     unimplemented!()
 }
 
